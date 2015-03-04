f1d90ad14587b53d8f53ff997f9f6030f77e4be6
Roy Cao <roycao@ZHOUYINGs-MacBook-Pro.local>
2015-03-04 01:55:30 -0800

    Add a button and a text.
    Signed-off-by: Roy Cao <roycao@ZHOUYINGs-MacBook-Pro.local>

--- coffeMe/coffeMe/ViewController.swift
+++ coffeMe/coffeMe/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var MyLabel: UILabel!
+    @IBAction func MyButton(sender: AnyObject) {
+        self.MyLabel.text = "Hello";
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
